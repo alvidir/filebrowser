79d8482b7302ee61023f0a49c502344531e390b2
Hector <thehector1593@gmail.com>
2023-01-11 19:04:52 +0100

    chore: remove unused error [skip ci]

--- errors.go
+++ errors.go
@@ -3,15 +3,14 @@ package filebrowser
 import "errors"
 
 var (
-	ErrUnknown          = errors.New("E001")
-	ErrNotFound         = errors.New("E002")
-	ErrNotAvailable     = errors.New("E003")
-	ErrUnauthorized     = errors.New("E004")
-	ErrInvalidToken     = errors.New("E005")
-	ErrInvalidFormat    = errors.New("E006")
-	ErrInvalidHeader    = errors.New("E007")
-	ErrWrongCredentials = errors.New("E008")
-	ErrAlreadyExists    = errors.New("E010")
+	ErrUnknown       = errors.New("E001")
+	ErrNotFound      = errors.New("E002")
+	ErrNotAvailable  = errors.New("E003")
+	ErrUnauthorized  = errors.New("E004")
+	ErrInvalidToken  = errors.New("E005")
+	ErrInvalidFormat = errors.New("E006")
+	ErrInvalidHeader = errors.New("E007")
+	ErrAlreadyExists = errors.New("E010")
 
 	ErrChannelClosed  = errors.New("channel closed")
 	ErrBlurredContent = errors.New("blurred content")
